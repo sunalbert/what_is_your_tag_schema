43249794b40d6bae10f71790ec99884dd74f7c42
jasonhavend <jasonhaven@163.com>
2019-06-13 10:56:28 +0800

    add code

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # what_is_your_label_system
 A script for looking the label system of data for sequence tagging task such as NER, Chunking, POS et.
+
+## Usage
+
+1.Just copy the script to the data folder will be work.
+
+2.Run command
+
+```shell
+python what_is_your_tag_system.py --file_path xxxx
+```
+
+## Result
+
+![](result.png)
---
+++ what_is_your_tag_system.py
@@ -0,0 +1,63 @@
+# coding=utf-8
+# /*
+#  * @Author: jasonhavend
+#  * @Date: 2019-06-13 10:02:40
+#  * @Last Modified by:   jasonhavend
+#  * @Last Modified time: 2019-06-13 10:02:40
+#  */
+import pandas as pd
+import argparse
+import os
+
+
+def look_only_one_column(col_list):
+    prefixs = set()
+    labels = set()
+    for tag in col_list[1:]:  # skip the first line
+        tag = str(tag)
+        if tag == 'O' or tag == 'o':
+            labels.add('O')
+        elif '-' in tag:
+            prefixs.add(tag.split('-')[0])
+            labels.add(tag.split('-')[1])
+        else:
+            labels.add(tag)
+    print('Prefix:', end=' ')
+    print(prefixs)
+    print('Label:', end=' ')
+    print(labels)
+
+
+def main(args):
+    # load data
+    if not os.path.exists(args.file_path):
+        print('{} not exists!'.format(args.file_path))
+        return
+    seps = [',', '\t', ' ']
+    data = None
+    for sep in seps:
+        try:
+            data = pd.read_csv(args.file_path, sep=sep)
+        except ValueError as e:
+            continue
+    if data is None:
+        print('The format of {} is not right for dataframe!')
+        return
+    data.columns = list(range(1, len(data.columns)+1))
+    for col in data.columns[1:]:
+        print('Look column = {}'.format(col))
+        look_only_one_column(data[col])
+        print()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # Required parameters
+    parser.add_argument("--file_path",
+                        default='train.csv',
+                        type=str,
+                        required=True,
+                        help="The input file path. Should can be read as the dataframe' format using 'pandas.read_csv'")
+    args = parser.parse_args()
+    main(args)
