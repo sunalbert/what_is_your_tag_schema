30e47162c668ef76f4d6e413cf80a014b7acc3e0
Jianghai.Dai <jasonhavend@gmail.com>
2019-06-15 21:56:04 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,5 +0,0 @@
-python what_is_your_tag_system.py --file_path conll2000_chunk.txt
-python what_is_your_tag_system.py --file_path conll2003_chunk.txt
-python what_is_your_tag_system.py --file_path conll2003_ner.txt
-python what_is_your_tag_system.py --file_path msra_ner.txt
-python what_is_your_tag_system.py --file_path chinese_ner.txt
